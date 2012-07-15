8aa1a0d5fc05a18c69f341e1503e3b3b15d0a5a8
yeehaa <yeehaa123@mac.com>
2012-07-15 19:02:33 +0200

    Add Cucumber features

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,7 @@
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
+include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-error', text: message)
 	end
 end
