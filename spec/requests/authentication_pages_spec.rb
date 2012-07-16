3b662f753477828123443ef5e26deeb3021ead2d
yeehaa <yeehaa123@mac.com>
2012-07-16 08:56:23 +0200

    improve sessions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
 describe "AuthenticationPages" do
+	before { visit signin_path }
 
 	subject { page }
 
 	describe "signin page" do
-		before { visit signin_path }
 
 		it { should have_selector('h1', 	text: 'Sign in') }
 		it { should have_selector('title', 	text: 'Sign in') }
 	end
 
 	describe "signin" do
-		before { visit signin_path }
 
 		describe "with invalid information" do
 			before { click_button "Sign in" }
-
+			
 			it { should have_selector('title', text: 'Sign in') }
 			it { should have_error_message('Invalid') }
 
@@ -28,11 +27,7 @@ describe "AuthenticationPages" do
 
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
- 			before do
- 				fill_in "Email", 		with: user.email
- 				fill_in "Password",		with: user.password
- 				click_button "Sign in"
- 			end
+ 			before { valid_signin(user) }
 
 			it { should have_selector('title', 		text: user.name) }
 			it { should have_link('Profile',		href: user_path(user)) }
