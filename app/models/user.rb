2f1f35238ca01f3d3b67b9b774a52342d7e48ce0
yeehaa <yeehaa123@mac.com>
2012-07-19 15:42:58 +0200

    add parts of relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
-
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
@@ -13,6 +14,14 @@ class User < ActiveRecord::Base
   					uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def following?(other_user)
+    self.relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
 
   def feed
     # This is only a proto-feed.
