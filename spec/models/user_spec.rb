c5ecbfacfe7e7e266240d54b196fb1364c3c8d6a
yeehaa <yeehaa123@mac.com>
2012-07-20 11:37:18 +0200

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,9 +20,13 @@ describe User do
 	it { should respond_to(:microposts) }
 	it { should respond_to(:feed) }
 	it { should respond_to(:relationships) }
+	it { should respond_to(:reverse_relationships) }
 	it { should respond_to(:followed_users)}
+	it { should respond_to(:followers) }
 	it { should respond_to(:following?) }
 	it { should respond_to(:follow!) }
+	it { should respond_to(:unfollow!) }
+
 
 	it { should be_valid }
 	it { should_not be_admin }
@@ -162,13 +166,24 @@ describe User do
 		end
 
 		describe "status" do
-		let(:unfollowed_post) do 
-			FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-		end
-		
-		its(:feed) { should include(older_micropost) }	
-		its(:feed) { should include(newer_micropost) }	
-		its(:feed) { should_not include(unfollowed_post) }	
+			let(:unfollowed_post) do 
+				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+			end
+			let(:followed_user) { FactoryGirl.create(:user) }
+
+			before do
+				@user.follow!(followed_user)
+				3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+			end
+
+			its(:feed) { should include(older_micropost) }	
+			its(:feed) { should include(newer_micropost) }	
+			its(:feed) { should_not include(unfollowed_post) }
+			its(:feed) do
+				followed_user.microposts.each do |micropost|
+					should include(micropost)
+				end
+			end
 		end
 	end
 
@@ -181,5 +196,17 @@ describe User do
 
 		it { should be_following(other_user) }
 		its(:followed_users) { should include(other_user) }
+
+		describe "followed user" do
+			subject { other_user }
+			its(:followers) { should include(@user) }
+		end
+
+		describe "unfollowing" do
+			before { @user.unfollow!(other_user) }
+
+			it { should_not be_following(other_user) }
+			its(:followed_users) { should_not include(other_user) }
+		end
 	end
 end
